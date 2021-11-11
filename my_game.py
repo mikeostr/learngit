7b8b2780bf5b7f82006ac5e300956b832eb01a57
mikeostr <mikhail.ostrovskiy@gmail.com>
2021-11-12 00:42:11 +0300

    попытка изучить pyGame

---
+++ my_game.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+WIDTH  = 360
+HEIGHT = 480
+FPS = 30
+pygame.init()
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+pygame.init()
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    screen.fill(BLACK)
+    pygame.display.flip()
+    
