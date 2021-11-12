efc4606d6ffdfdc8a6639cc70bffd79b84dddf87
mikeostr <mikhail.ostrovskiy@gmail.com>
2021-11-13 00:16:27 +0300

    попытка изучить pyGame

--- my_game.py
+++ my_game.py
@@ -1,13 +1,13 @@
 import pygame
 import random
-WIDTH  = 360
+WIDTH = 360
 HEIGHT = 480
 FPS = 30
-pygame.init()
-pygame.mixer.init()  # для звука
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 pygame.init()
 pygame.mixer.init()  # для звука
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,11 +16,13 @@ clock = pygame.time.Clock()
 
 running = True
 while running:
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
     screen.fill(BLACK)
     pygame.display.flip()
-    
+    for i in (BLACK, BLUE, WHITE, RED, GREEN):
+        screen.fill(i)
+        pygame.display.flip()
+pygame.quit()
---
+++ primer1.py
@@ -0,0 +1,34 @@
+def is_prime(number: int) -> bool:
+    """ Возвращает True, если number - это простое число
+    """
+    assert number > 1
+    return all(number % i for i in range(2, int(number**0.5) + 1))
+
+
+def is_fibonacci(number: int) -> bool:
+    """ Возвращает True, если number - это число Фибоначчи
+    """
+    assert number > 1
+    a, b = 0, 1
+    while a + b < number:
+        a, b = b, a + b
+    return a + b == number
+
+
+# Множество простых чисел до 100
+primes = set(filter(is_prime, range(2, 101)))
+print(primes)
+
+# Множество чисел Фибоначчи до 100
+fibonacci = set(filter(is_fibonacci, range(2, 101)))
+
+# Множество простых чисел до 100, которые одновременно являются
+# числами Фибоначчи
+prime_fibonacci = primes.intersection(fibonacci)
+print(prime_fibonacci)
+
+# Или используя оператор `&`, который определён для множеств
+prime_fibonacci = fibonacci & primes
+
+
+print(prime_fibonacci)
